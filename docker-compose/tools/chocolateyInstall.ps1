1935aa870af789f34b4e6a8a2521db39d4b485ff
Miguel Allende <105422197+mallendem@users.noreply.github.com>
2026-01-30 12:06:54 +0100

    update plugin installation and uninstallation paths (#7)
    * Update plugin installation and uninstallation paths to use the correct directory
    
    * Remove deprecated paths when installing

--- docker-compose/tools/chocolateyInstall.ps1
+++ docker-compose/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $packageToolsDir        = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $executablePath         = Join-Path $packageToolsDir 'docker-compose.exe'
 
-$executableTargetDir    = 'C:\ProgramData\Docker\cli-plugins\'
+$executableTargetDir    = 'C:\Program Files\Docker\cli-plugins\'
 $executableTargetPath   = Join-Path $executableTargetDir 'docker-compose.exe'
 
 # create plugin directory if it doesn't exist
@@ -12,6 +12,29 @@ if (-not (Test-Path -Path $executableTargetDir)) {
 
 # remove docker-compose shim if it still exists
 Uninstall-BinFile -name docker-compose -ErrorAction:Ignore
+
+# clean up old installation location (backward compatibility)
+$oldExecutableDir  = 'C:\ProgramData\Docker\cli-plugins\'
+$oldExecutablePath = Join-Path $oldExecutableDir 'docker-compose.exe'
+$oldDockerPath     = Split-Path -Path $oldExecutableDir -Parent
+
+if (Test-Path -Path $oldExecutablePath) {
+    Remove-Item -Path $oldExecutablePath -Force
+}
+
+# remove old plugin directory if empty
+if (Test-Path -Path $oldExecutableDir) {
+    if ($null -eq (Get-ChildItem -Path $oldExecutableDir)) {
+        Remove-Item -Path $oldExecutableDir -Force
+    }
+}
+
+# remove old docker directory if empty
+if (Test-Path -Path $oldDockerPath) {
+    if ($null -eq (Get-ChildItem -Path $oldDockerPath)) {
+        Remove-Item -Path $oldDockerPath -Force
+    }
+}
 
 # move executable
 Move-Item -Path $executablePath -Destination $executableTargetPath -Force
