08158b0c02fe016fcc48f12e9e375d41d6d2e559
Miguel Allende <miguel.allende@elastic.co>
2026-01-30 11:06:14 +0100

    Remove deprecated paths when installing

--- docker-compose/tools/chocolateyInstall.ps1
+++ docker-compose/tools/chocolateyInstall.ps1
@@ -16,13 +16,23 @@ Uninstall-BinFile -name docker-compose -ErrorAction:Ignore
 # clean up old installation location (backward compatibility)
 $oldExecutableDir  = 'C:\ProgramData\Docker\cli-plugins\'
 $oldExecutablePath = Join-Path $oldExecutableDir 'docker-compose.exe'
+$oldDockerPath     = Split-Path -Path $oldExecutableDir -Parent
+
 if (Test-Path -Path $oldExecutablePath) {
     Remove-Item -Path $oldExecutablePath -Force
-    # remove old directory if empty
-    if (Test-Path -Path $oldExecutableDir) {
-        if ($null -eq (Get-ChildItem -Path $oldExecutableDir)) {
-            Remove-Item -Path $oldExecutableDir -Force
-        }
+}
+
+# remove old plugin directory if empty
+if (Test-Path -Path $oldExecutableDir) {
+    if ($null -eq (Get-ChildItem -Path $oldExecutableDir)) {
+        Remove-Item -Path $oldExecutableDir -Force
+    }
+}
+
+# remove old docker directory if empty
+if (Test-Path -Path $oldDockerPath) {
+    if ($null -eq (Get-ChildItem -Path $oldDockerPath)) {
+        Remove-Item -Path $oldDockerPath -Force
     }
 }
 
